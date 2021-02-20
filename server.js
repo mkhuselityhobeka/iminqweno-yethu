fdce166df33ac81a9568f1f1920f9c64994e0c72
mkhuselityhobeka@gmail.com <mkhuselityhobeka@gmail.com>
2021-02-20 14:27:44 +0200

    changed output path in server.js file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/funda-registration-front-end'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('/app/app.component.html', {root: 'dist/FundaRegistration-Front-End/'}),
+    res.sendFile('app/app.component.html', {root: 'dist/FundaRegistration-Front-End/'}),
 );
 
 app.listen(process.env.PORT || 8080);
